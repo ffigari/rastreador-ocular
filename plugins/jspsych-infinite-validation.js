a1f608612a5c7ab21acd37b98c69946cd4eb8b1a
Francisco Figari <figafran@gmail.com>
2021-09-07 15:20:59 -0300

    Display clearer message

--- plugins/jspsych-infinite-validation.js
+++ plugins/jspsych-infinite-validation.js
@@ -6,6 +6,8 @@ jsPsych.plugins['infinite-validation'] = (function(){
     trial: async function(display_element, trial) {
       const estimator = await eyeTracking.switchTo.estimating()
 
+      estimator.showVisualization()
+
       await displayHTML(`
         <p>
           Vamos a realizar continuamente rondas de validación. Luego de cada una
@@ -18,21 +20,27 @@ jsPsych.plugins['infinite-validation'] = (function(){
       while (true) {
         collectedMetrics.push(await estimator.runValidationRound(drawer))
 
-        // TODO: Display message
-        // TODO: Show plot
         const spaceWasPressed = await displayHTML(`
-          <ul>
-            ${collectedMetrics.map((metrics) => {
-              return `<li>${metrics.average.linearError()}</li>`
-            })}
-          </ul>
+          <div>
+            Resultados obtenidos hasta ahora:
+            <ul>
+              ${collectedMetrics.map((metrics) => {
+                return `<li>${metrics.average.linearError()}</li>`
+              }).join('')}
+            </ul>
+          </div>
+          <p>
+            Para realizar otra ronda de validación presioná la barra de espacio.
+            <br>
+            Para terminar presioná cualquier otro botón.
+          </p>
         `).at(display_element).untilAnyKeyIsPressed()
 
-        if (spaceWasPressed) {
-          break
-        }
+        if (spaceWasPressed) continue
+        else break
       }
 
+      estimator.hideVisualization()
       eyeTracking.switchTo.idle()
       jsPsych.finishTrial();
     },
