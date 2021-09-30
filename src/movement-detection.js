ec553b47e4167cd16f89e60979ae09a17f78446e
Francisco Figari <figafran@gmail.com>
2021-09-30 12:59:04 -0300

    Refactor eye patches out of main section

--- src/movement-detection.js
+++ src/movement-detection.js
@@ -1,6 +1,49 @@
-
-
 const movementDetector = (function() {
+  const create = {
+    eyePatch: (prediction, keypointsIndexes) => {
+      const min = { x: null, y: null }
+      const max = { x: null, y: null };
+      keypointsIndexes.forEach(keypointIndex => {
+        const [x, y, z] = prediction.scaledMesh[keypointIndex];
+        min.x = min.x && min.x < x ? min.x : x;
+        min.y = min.y && min.y < y ? min.y : y;
+        max.x = max.x && max.x > x ? max.x : x;
+        max.y = max.y && max.y > y ? max.y : y;
+      })
+      return new (function (min, max) {
+        Object.assign(this, {
+          min, max, width: max.x - min.x, height: max.y - min.y,
+        }, {
+          drawItselfOver(ctx, color) {
+            ctx.lineWidth = 2;
+            ctx.strokeStyle = color;
+            ctx.strokeRect(this.min.x, this.min.y, this.width, this.height);
+          },
+        })
+      })(min, max);
+    },
+    eyesPatchsPair: (prediction) => {
+      // Los índices de los keypoints pueden consultarse en
+      // https://github.com/tensorflow/tfjs-models/blob/master/face-landmarks-detection/mesh_map.jpg
+      const [leftEyePatch, rightEyePatch] = [
+        [189, 244, 232, 230, 228, 226, 225, 223, 221],
+        [413, 464, 452, 450, 448, 446, 445, 443, 441],
+      ].map(keypointsIndexes => create.eyePatch(prediction, keypointsIndexes))
+      return new (function (left, right) {
+        Object.assign(this, {
+          left, right
+        }, {
+          drawItselfOver(ctx, { leftColor, rightColor, color }) {
+            [
+              [this.left,  leftColor  || color || 'black'],
+              [this.right, rightColor || color || 'black'],
+            ].forEach(([patch, color]) => patch.drawItselfOver(ctx, color))
+          },
+        })
+      })(leftEyePatch, rightEyePatch);
+    },
+  }
+
   const module = {}
   const state = {
     calibrationInProgress: false,
@@ -45,58 +88,28 @@ const movementDetector = (function() {
             state.debuggingCanvasCtx.canvas.width,
             state.debuggingCanvasCtx.canvas.height
           );
-          state.collectedEyesPatches.forEach(({ left, right }) => {
-            [[left, 'green'], [right, 'blue']].forEach(([{ min, max }, color]) => {
-              const width = max.x - min.x;
-              const height = max.y - min.y;
-              state.debuggingCanvasCtx.lineWidth = 2;
-              state.debuggingCanvasCtx.strokeStyle = color;
-              state.debuggingCanvasCtx.strokeRect(min.x, min.y, width, height);
-            });
+          state.collectedEyesPatches.forEach((x) => x.drawItselfOver(
+            state.debuggingCanvasCtx, { leftColor: 'green', rightColor: 'blue', }
+          ));
+          state.lastCapturedEyes?.drawItselfOver(state.debuggingCanvasCtx, {
+            color: 'red',
           })
-          if (state.lastCapturedEyes) {
-            const { left, right } = state.lastCapturedEyes;
-            [left, right].forEach(({ min, max }) => {
-              const width = max.x - min.x;
-              const height = max.y - min.y;
-              state.debuggingCanvasCtx.lineWidth = 2;
-              state.debuggingCanvasCtx.strokeStyle = 'red';
-              state.debuggingCanvasCtx.strokeRect(min.x, min.y, width, height);
-            });
-          }
         }
         const predictions = await model.estimateFaces({
           input: videoElement
         })
+        // TODO: En lugar de tirar un error habría que informar al usuario con
+        //       algún evento.
         if (predictions.length === 0) {
           throw new Error('No se detectó ninguna cara.')
         }
         if (predictions.length > 1) {
           throw new Error('Se detectó más de una cara.')
         }
-        const [leftBBox, rightBBox] = [
-          [189, 244, 232, 230, 228, 226, 225, 223, 221],
-          [413, 464, 452, 450, 448, 446, 445, 443, 441],
-        ].map(keypointIndexes => {
-          const min = { x: null, y: null }
-          const max = { x: null, y: null };
-          keypointIndexes.forEach(keypointIndex => {
-            const [x, y, z] = predictions[0].scaledMesh[keypointIndex];
-            min.x = min.x && min.x < x ? min.x : x;
-            min.y = min.y && min.y < y ? min.y : y;
-            max.x = max.x && max.x > x ? max.x : x;
-            max.y = max.y && max.y > y ? max.y : y;
-          })
-          return { min, max }
-        })
-
-        state.lastCapturedEyes = { left: leftBBox, right: rightBBox };
-
+        const eyesPatchsPair = create.eyesPatchsPair(predictions[0]);
+        state.lastCapturedEyes = eyesPatchsPair;
         if (state.useNextFrameAsValidPosition) {
-          state.collectedEyesPatches.push({
-            left: leftBBox,
-            right: rightBBox,
-          });
+          state.collectedEyesPatches.push(eyesPatchsPair);
           state.useNextFrameAsValidPosition = false;
         }
 
