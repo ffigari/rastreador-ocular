7382597a4f40a404e6423bd4907b3f291d3b6c9c
Francisco Figari <figafran@gmail.com>
2021-11-11 12:01:35 -0300

    Check minimum size of viewport

--- plugins/check-requirements.js
+++ plugins/check-requirements.js
@@ -4,13 +4,17 @@ jsPsych.plugins['check-requirements'] = (function(){
       name: 'check-requirements',
     },
     trial: async function(display_element, trial) {
-      if (!requirementsChecker.requirementsAreMet()) {
+      const { systemIsOk, errors } = requirementsChecker.checkSystem()
+      if (!systemIsOk) {
         display_element.innerHTML = `
           <h2>Hardware insuficiente</h2>
           <p>
             Tu sistema no cumple con los requerimientos de hardware necesarios
             por lo que no podremos continuar con el experimento.
           </p>
+          <ul>
+            ${errors.map(e => `<li>${e}</li>`).join('')}
+          </ul>
         `;
       } else {
         jsPsych.finishTrial();
--- src/requirements-checker.js
+++ src/requirements-checker.js
@@ -1,7 +1,35 @@
 const requirementsChecker = (function () {
+  const MINIMUM_HEIGHT = 500;
+  const MINIMUM_WIDTH = 700;
+
   return {
-    requirementsAreMet() {
-      return false;
+    checkSystem() {
+      let errors = [];
+
+      const viewportWidth = Math.max(
+        document.documentElement.clientWidth || 0,
+        window.innerWidth || 0
+      );
+      const viewportHeight = Math.max(
+        document.documentElement.clientHeight || 0,
+        window.innerHeight || 0
+      );
+      const viewportSizeIsRespected = 
+        viewportHeight > MINIMUM_HEIGHT &&
+        viewportWidth > MINIMUM_HEIGHT;
+
+      !viewportSizeIsRespected && errors.push(
+        `La pantalla debe tener al menos una resolución de ${
+          MINIMUM_WIDTH
+        }x${
+          MINIMUM_HEIGHT
+        }.`
+      );
+
+      return {
+        systemIsOk: viewportSizeIsRespected,
+        errors,
+      };
     }
   };
 })();
