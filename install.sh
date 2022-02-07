48ad08931d4e89418ba33feaf01cd73bd3ef2af0
Francisco Figari <figafran@gmail.com>
2022-02-07 17:47:40 -0300

    Update install script to fetch and build webgazer fork

--- install.sh
+++ install.sh
@@ -5,6 +5,21 @@ INSTALL_PATH=www/vendor
 rm -rf $INSTALL_PATH
 mkdir $INSTALL_PATH
 
+# webgazer
+rm -f ./$INSTALL_PATH/webgazer.js
+rm -rf ./webgazer
+git clone -b develop https://github.com/ffigari/WebGazer webgazer
+
+(
+  . ~/.nvm/nvm.sh
+  nvm use 14
+  cd webgazer
+  npm i
+  npm run build
+  cp dist/webgazer.js ../$INSTALL_PATH/
+)
+rm -rf ./webgazer
+
 # Psychophysics
 PSYCHOPHYSICS_VERSION=3.1.0
 PSYCHOPHYSICS_ZIP_NAME=jspsych-psychophysics-$PSYCHOPHYSICS_VERSION.zip
@@ -16,6 +31,4 @@ unzip $INSTALL_PATH/$PSYCHOPHYSICS_ZIP_NAME \
   -d $INSTALL_PATH
 rm $INSTALL_PATH/$PSYCHOPHYSICS_ZIP_NAME
 
-echo Done installing JSPsych-Psychophysics $PSYCHOPHYSICS_VERSION
-
-# TODO: Checkout wg fork, compile into www directory
+echo Done installing WebGazer fork and JSPsych-Psychophysics $PSYCHOPHYSICS_VERSION
