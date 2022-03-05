9350cf038a52d138ca1dd55b1896bd82148ca1be
Francisco Figari <figafran@gmail.com>
2022-03-05 17:54:50 -0300

    Mirror, center and normalize x data

--- src/data-analysis/main.py
+++ src/data-analysis/main.py
@@ -15,6 +15,7 @@ for file_path in os.listdir(antisaccades_data_path):
         csv_rows_iterator = csv.reader(f, delimiter=",", quotechar='"')
         headers = next(csv_rows_iterator, None)
 
+        trial_id_idx = headers.index('trialId')
         inner_width_idx = headers.index('inner_width')
         wg_data_idx = headers.index('webgazer_data')
         exp_name_idx = headers.index('experimentName')
@@ -29,15 +30,18 @@ for file_path in os.listdir(antisaccades_data_path):
 
         run_trials = []
         inner_width = None
+        # TODO: Hay que saltarse los trials iniciales de prueba
         for row in csv_rows_iterator:
             if row[inner_width_idx] != '"':
                 inner_width = json.loads(row[inner_width_idx])
             if row[exp_name_idx] == "antisaccade":
                 run_trials.append({
+                    "trial_id": json.loads(row[trial_id_idx]),
                     "gaze_estimations": json.loads(row[wg_data_idx]),
                     "center_x": json.loads(row[center_x_idx]),
                     "center_y": json.loads(row[center_y_idx]),
                     "cue_show_at_left": json.loads(row[cue_shown_at_left_idx]),
+                    "cue_abs_x_delta": abs(json.loads(row[cue_x_delta_idx])),
                     "pre_duration": json.loads(row[pre_trial_duration_idx]),
                     "fixation_duration": json.loads(row[fixation_duration_idx]),
                     "mid_duration": json.loads(row[mid_blank_duration_idx]),
@@ -64,14 +68,28 @@ for file_path in os.listdir(antisaccades_data_path):
 
 def normalize(trial):
     delta_xs = []
-    print(trial)
     for g in trial["gaze_estimations"]:
-        # TODO: Normalize and center
-        print(g['x'], trial['center_x'], trial['inner_width'])
-        asd
+        # Center, normalize and mirror data
+        # To prevent having to take into account the side in which the visual
+        # cue is shown, we mirror the data when it gets shown in the left side.
+        # Then the coordinate gets also centered and normalized so that we can
+        # assume that on every trial and for every subject the coordinate in 
+        # which the stimulus was shown is x = 1
+        x = (
+            -1 if trial['cue_show_at_left'] else 1
+        ) * (g['x'] - trial['center_x']) / trial['cue_abs_x_delta']
+        delta_xs.append({ 'x': x, 't': g['t'] })
     return {
         "delta_xs": delta_xs
     }
 
 d = [normalize(t) for t in trials]
-
+fig, ax = plt.subplots()
+for t in d:
+    ax.plot(
+        [e['t'] for e in t['delta_xs']],
+        [e['x'] for e in t['delta_xs']],
+        alpha=0.4,
+        linewidth=0.7
+    )
+plt.show()
