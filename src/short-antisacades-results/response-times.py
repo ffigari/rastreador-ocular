589614a711d436e71df0fddfa248a315f38fb2dd
Francisco Figari <figafran@gmail.com>
2022-03-17 20:22:32 -0300

    Mirror data to simulate stimulus appears always at x = 1

--- src/short-antisacades-results/response-times.py
+++ src/short-antisacades-results/response-times.py
@@ -15,6 +15,26 @@ for t in trials:
     )
 plt.show()
 
-# TODO: Mirror data
+def mirror_trial(t):
+    if t['cue_shown_at_left']:
+        for e in t['estimations']:
+            e['x'] *= -1
+    return t
+
+trials = [mirror_trial(t) for t in trials]
+fix, ax = plt.subplots()
+for t in trials:
+    if t['outlier']:
+        raise Exception("outliers should have been filtered out at this point")
+    ax.plot(
+        [e['t'] for e in t['estimations']],
+        [e['x'] for e in t['estimations']],
+        color="black",
+        alpha=0.1
+    )
+plt.show()
 
 # TODO: Compute response times and correcteness of trial
+#       Plot them in a grid where one axis is the response time and the other
+#       one is the correcteness
+
