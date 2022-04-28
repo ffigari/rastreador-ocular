469160ecf47b29c65c81bfb09ccea90a3e77b24e
Francisco Figari <figafran@gmail.com>
2022-04-28 19:28:34 -0300

    Parse validation results

--- src/antisacadas-results/main.py
+++ src/antisacadas-results/main.py
@@ -12,23 +12,60 @@ for file_name in os.listdir(data_path):
         headers = next(csv_rows_iterator, None)
 
         trial_index_idx = headers.index('trial_index')
-        type_idx = headers.index('type')
+        type_idx = headers.index('rastoc-type')
         center_x_idx = headers.index('center_x')
-        results_idx = headers.index('results')
-        validation_succeded_idx = headers.index('validationSucceded')
+        stimulus_coordinate_idx = headers.index('stimulus-coordinate')
+        validation_id_idx = headers.index('validation-id')
+        validation_point_id_idx = headers.index('validation-point-id')
+        validation_results_idx = headers.index('validation-results')
+        validation_last_results_idx = headers.index('last-estimations')
 
         # Only coordinate x will be parsed and normalized since we don't need to
         # analyze vertical coordinate
         normalizer = None
         validations_count = 0
-        for row in csv_rows_iterator:
-            if (row[type_idx] != ''):
-                center_x = int(row[center_x_idx])
-                results = json.loads(row[results_idx])
-                ui_validation_succeded = row[validation_succeded_idx]
-                print('\n%d-th validation (ui success? %s)' % (
-                    validations_count,
-                    ui_validation_succeded
-                ))
-                normalizer = Normalizer(center_x, results)
-                validations_count += 1
+
+        while True:
+            row = next(csv_rows_iterator, None)
+            if row is None:
+                break
+
+            # If a validation is found, consume its rows to update the
+            # coordinates normalizer
+            if row[type_idx] == 'validation-stimulus':
+                stimulus_results = []
+                validation_was_successful = None
+                while True:
+                    stimulus_delta = \
+                        json.loads(row[stimulus_coordinate_idx])['x']
+                    stimulus_results.append({
+                        'position': \
+                            'center' if stimulus_delta == 0 else \
+                            'left' if stimulus_delta < 0 else \
+                            'right',
+                        'real-stimulus-x': \
+                            int(row[center_x_idx]) + stimulus_delta,
+                        'last-estimates': \
+                            json.loads(row[validation_last_results_idx])
+                    })
+                    # The last row concerning this validation contains its
+                    # results
+                    if int(row[validation_point_id_idx]) == 5:
+                        validation_was_successful = json.loads(row[
+                            validation_results_idx
+                        ])['relativePositionsAreCorrect']
+                    row = next(csv_rows_iterator, None)
+                    if row[type_idx] != 'validation-stimulus':
+                        normalizer = Normalizer(
+                            stimulus_results,
+                            validation_was_successful
+                        )
+                        break
+            print(
+                run_id,
+                row[trial_index_idx]
+            )
+            # TODO: Parse trials
+            #         - Estimations should be interpolated and normalized
+            #         - Trials from the tutorial should not be included in the
+            #           analysis but its validation must be considered
--- src/antisacadas-results/normalizer.py
+++ src/antisacadas-results/normalizer.py
@@ -1,57 +1,26 @@
 class Normalizer:
-    def __init__(self, center_x, results):
-        delta_width = round(center_x * 2 / 3)
-        first_center_result = results[0]
-        last_center_result = results[-1]
-        top_left_result = [
-            r for r in results if r['step'] == { 'x': -1, 'y': -1 }
-        ][0]
-        bot_left_result = [
-            r for r in results if r['step'] == { 'x': -1, 'y': 1 }
-        ][0]
-        top_right_result = [
-            r for r in results if r['step'] == { 'x': 1, 'y': -1 }
-        ][0]
-        bot_right_result = [
-            r for r in results if r['step'] == { 'x': 1, 'y': 1 }
-        ][0]
-        real_left_validation_x = center_x - delta_width
-        real_right_validation_x = center_x + delta_width
+    def __init__(self, stimulus_results, validation_was_successful):
+        print('producing new normalizer', validation_was_successful)
+        [print(s) for s in stimulus_results]
 
-        print('real_left_x: %d; top_left_x: %d, bot_left_x: %d' % (
-            real_left_validation_x,
-            top_left_result['avgX'],
-            bot_left_result['avgX']
-        ))
-        print('real_right_x: %d; top_right_x: %d, bot_right_x: %d' % (
-            real_right_validation_x,
-            top_right_result['avgX'],
-            bot_right_result['avgX']
-        ))
-        print('real_center_x: %d; first_center_x: %d, second_center_x: %d' % (
-            center_x,
-            first_center_result['avgX'],
-            last_center_result['avgX']
-        ))
-        left_estimation_goes_beyond_center_estimation = \
-            top_left_result['avgX'] > first_center_result['avgX'] or \
-            top_left_result['avgX'] > last_center_result['avgX'] or \
-            bot_left_result['avgX'] > first_center_result['avgX'] or \
-            bot_left_result['avgX'] > last_center_result['avgX']
-        right_estimation_goes_beyond_center_estimation = \
-            top_right_result['avgX'] < first_center_result['avgX'] or \
-            top_right_result['avgX'] < last_center_result['avgX'] or \
-            bot_right_result['avgX'] < first_center_result['avgX'] or \
-            bot_right_result['avgX'] < last_center_result['avgX']
-        print(
-            'left_estimation_goes_beyond_center_estimation:',
-            left_estimation_goes_beyond_center_estimation
-        )
-        print(
-            'right_estimation_goes_beyond_center_estimation:',
-            right_estimation_goes_beyond_center_estimation
-        )
+        regions_of_interest = ['center', 'left', 'right']
+        validated_positions_average_x_estimate = dict()
+        validated_positions_real_x = dict()
+        for p in regions_of_interest:
+            validated_positions_average_x_estimate[p] = 0
+        for p in regions_of_interest:
+            for r in [r for r in stimulus_results if r['position'] == p]:
+                validated_positions_real_x[p] = r['real-stimulus-x']
+                es = [e['x'] for e in r['last-estimates']]
+                validated_positions_average_x_estimate[p] += sum(es) / len(es)
+        for p in regions_of_interest:
+            validated_positions_average_x_estimate[p] /= 2
 
-        self.validation_was_sketchy = \
-            left_estimation_goes_beyond_center_estimation or \
-            right_estimation_goes_beyond_center_estimation
+        # TODO: Real coordinates (eg, the ones from stimulus) and estimated
+        #       coordinates should be normalized distinctly using the two
+        #       variables below
+        print(validated_positions_average_x_estimate)
+        print(validated_positions_real_x)
+
+        self.is_sketchy_normalizer = not validation_was_successful
+        print('boop')
