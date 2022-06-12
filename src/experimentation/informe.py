b08c0683951c89c4f0be2ee4d98f5127fa5017e0
Francisco Figari <figafran@gmail.com>
2022-06-12 20:19:37 -0300

    Allow individual plots

--- src/experimentation/informe.py
+++ src/experimentation/informe.py
@@ -12,16 +12,13 @@ def parse_instances():
         'second': parse_second_instance()
     }
 
-def draw_metric(instances, metric_name, field_name):
-    for j, name in enumerate(['first', 'second']):
-        perRunAx = axes[0][j]
-        perTrialAx = axes[1][j]
-        separated_hist(
-            perRunAx,
-            perTrialAx,
-            instances[name]['post_filtering_metrics'][metric_name],
-            field_name
-        )
+def draw_compared_metric(instance, perRunAx, perTrialAx, metric_name, field_name):
+    separated_hist(
+        perRunAx,
+        perTrialAx,
+        instance['post_filtering_metrics'][metric_name],
+        field_name
+    )
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -53,30 +50,65 @@ if __name__ == "__main__":
         )
         sys.exit(-1)
 
+    scope = 'both'
+    if len(sys.argv) >= 3:
+        if sys.argv[2] == 'first':
+            scope = 'first'
+        elif sys.argv[2] == 'second':
+            scope = 'second'
+        else:
+            print('unkown scope', file=sys.stderr)
+            sys.exit(-1)
+
     instances = parse_instances()
+    
+    nrows = 2
+    ncols = 1
+    if scope == 'both':
+        ncols = 2
 
-    fig, axes = plt.subplots(nrows=2, ncols=2)
+    fig, axes = plt.subplots(nrows=nrows, ncols=ncols)
     fig.suptitle(config[action]['title'])
-    draw_metric(
-        instances,
-        config[action]['metric_name'],
-        config[action]['key_name']
-    )
-    axes[0][0].set_ylabel('Cantidad de sujetos')
-    axes[1][0].set_ylabel('Cantidad de repeticiones')
-    axes[1][0].set_xlabel(config[action]['unit_label'])
-    axes[1][1].set_xlabel(config[action]['unit_label'])
+    if scope == 'both':
+        for j, name in enumerate(['first', 'second']):
+            draw_compared_metric(
+                instances[name],
+                axes[0][j],
+                axes[1][j],
+                config[action]['metric_name'],
+                config[action]['key_name']
+            )
+        axes[0][0].set_ylabel('Cantidad de sujetos')
+        axes[1][0].set_ylabel('Cantidad de repeticiones')
+        axes[1][0].set_xlabel(config[action]['unit_label'])
+        axes[1][1].set_xlabel(config[action]['unit_label'])
+    
+        axes[0][0].set_title('Primera instancia, repeticiones agrupadas por sujeto')
+        axes[1][0].set_title('Primera instancia, repeticiones miradas individualmente')
+        axes[0][1].set_title('Segunda instancia, repeticiones agrupadas por sujeto')
+        axes[1][1].set_title('Segunda instancia, repeticiones miradas individualmente')
+        if action == 'frequencies':
+            draw_sampling_frequecies_marks(axes[0][0])
+            draw_sampling_frequecies_marks(axes[1][0])
+            draw_sampling_frequecies_marks(axes[0][1])
+            draw_sampling_frequecies_marks(axes[1][1])
+        axes[0][1].legend()
+    else:
+        draw_compared_metric(
+            instances[scope],
+            axes[0],
+            axes[1],
+            config[action]['metric_name'],
+            config[action]['key_name']
+        )
+        axes[0].set_ylabel('Cantidad de sujetos')
+        axes[1].set_ylabel('Cantidad de repeticiones')
+        axes[1].set_xlabel(config[action]['unit_label'])
 
-    axes[0][0].set_title('Primera instancia, repeticiones agrupadas por sujeto')
-    axes[1][0].set_title('Primera instancia, repeticiones miradas individualmente')
-    axes[0][1].set_title('Segunda instancia, repeticiones agrupadas por sujeto')
-    axes[1][1].set_title('Segunda instancia, repeticiones miradas individualmente')
-    if action == 'frequencies':
-        draw_sampling_frequecies_marks(axes[0][0])
-        draw_sampling_frequecies_marks(axes[1][0])
-        draw_sampling_frequecies_marks(axes[0][1])
-        draw_sampling_frequecies_marks(axes[1][1])
-    axes[0][1].legend()
+        instance_label = 'Primera' if scope == 'first' else 'Segunda'
+        axes[0].set_title('{} instancia, repeticiones agrupadas por sujeto'.format(instance_label))
+        axes[1].set_title('{} instancia, repeticiones miradas individualmente'.format(instance_label))
+        axes[0].legend()
 
     plt.show()
 
