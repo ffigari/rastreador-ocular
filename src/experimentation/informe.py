560c2a66dc47bf47d63c8962a72e0b181e701d89
Francisco Figari <figafran@gmail.com>
2022-06-12 19:39:32 -0300

    Reuse frequencies plot for widths

--- src/experimentation/informe.py
+++ src/experimentation/informe.py
@@ -12,44 +12,63 @@ def parse_instances():
         'second': parse_second_instance()
     }
 
-def draw_metric(instances):
+def draw_metric(instances, metric_name, field_name):
     for j, name in enumerate(['first', 'second']):
         perRunAx = axes[0][j]
         perTrialAx = axes[1][j]
-        frequencies = instances[name]['post_filtering_metrics']['frequencies']
-        separated_hist(perRunAx, perTrialAx, frequencies, 'frequency')
+        separated_hist(
+            perRunAx,
+            perTrialAx,
+            instances[name]['post_filtering_metrics'][metric_name],
+            field_name
+        )
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print('missing action', file=sys.stderr)
         sys.exit(-1)
 
+    config = { 'frequencies': {
+        'title': 'Distribución de frecuencias de sampleo',
+        'metric_name': 'frequencies',
+        'key_name': 'frequency'
+    }, 'resolutions': {
+        'title': 'Distribución de frecuencias de anchos de pantalla',
+        'metric_name': 'widths',
+        'key_name': 'width'
+    }}
+
+    action = sys.argv[1]
+    if action not in config.keys():
+        print(
+            'unkown action, valid ones are [{}]'.format(', '.join(config.keys())),
+            file=sys.stderr
+        )
+        sys.exit(-1)
+
     instances = parse_instances()
-    fig, axes = plt.subplots(nrows=2, ncols=2, sharex=True)
-    if sys.argv[1] == 'frequencies':
-        fig.suptitle('Distribución de frecuencias de sampleo')
-        draw_metric(instances)
 
-        axes[0][0].set_ylabel('Cantidad de sujetos')
-        axes[1][0].set_ylabel('Cantidad de repeticiones')
+    fig, axes = plt.subplots(nrows=2, ncols=2)
+    fig.suptitle(config[action]['title'])
+    draw_metric(instances, config[action]['metric_name'], config[action]['key_name'])
+    axes[0][0].set_ylabel('Cantidad de sujetos')
+    axes[1][0].set_ylabel('Cantidad de repeticiones')
+    unit_label = \
+        'Frecuencia (en Hz)' if action == 'frequencies' else \
+        'Ancho de pantalla (en px)'
+    axes[1][0].set_xlabel(unit_label)
+    axes[1][1].set_xlabel(unit_label)
 
-        axes[1][0].set_ylabel('Frecuencia (en Hz)')
-        axes[1][1].set_ylabel('Frecuencia (en Hz)')
-
-        axes[0][0].set_title('Primera instancia, repeticiones agrupadas por sujeto')
-        axes[1][0].set_title('Primera instancia, repeticiones miradas individualmente')
-        axes[0][1].set_title('Segunda instancia, repeticiones agrupadas por sujeto')
-        axes[1][1].set_title('Segunda instancia, repeticiones miradas individualmente')
-
+    axes[0][0].set_title('Primera instancia, repeticiones agrupadas por sujeto')
+    axes[1][0].set_title('Primera instancia, repeticiones miradas individualmente')
+    axes[0][1].set_title('Segunda instancia, repeticiones agrupadas por sujeto')
+    axes[1][1].set_title('Segunda instancia, repeticiones miradas individualmente')
+    if action == 'frequencies':
         draw_sampling_frequecies_marks(axes[0][0])
         draw_sampling_frequecies_marks(axes[1][0])
         draw_sampling_frequecies_marks(axes[0][1])
         draw_sampling_frequecies_marks(axes[1][1])
-
-        axes[0][1].legend()
-
-    elif sys.argv[1] == 'resolutions':
-        print('TODO: Make figure with screen resolution per run and per trial')
+    axes[0][1].legend()
 
     plt.show()
 
