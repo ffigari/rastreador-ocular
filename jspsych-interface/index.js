acb64d5b37a0650bc30395aa517581df7d452f9d
Francisco Figari <figafran@gmail.com>
2022-08-29 11:44:12 -0300

    Add callbacks to ensureCalibration

--- jspsych-interface/index.js
+++ jspsych-interface/index.js
@@ -29,7 +29,7 @@ class EventsTrackingStart {
   }
   trial() {
     rastoc.startTrackingEvents();
-    jsPsych.finishTrial({
+    this.jsPsych.finishTrial({
       jspsych_start_time: this.jsPsych.getStartTime().toISOString(),
     })
   }
@@ -44,7 +44,7 @@ class EventsTrackingStop {
     this.jsPsych = jsPsych;
   }
   trial() {
-    jsPsych.finishTrial({
+    this.jsPsych.finishTrial({
       events: rastoc.stopTrackingEvents(),
     });
   }
@@ -377,6 +377,8 @@ const ensureCalibration = (options) => {
   options.forceCalibration = options.forceCalibration || false;
   options.performValidation = options.performValidation || false;
   options.maxRetries = options.maxRetries || 3;
+  options.postCalibrationCb = options.postCalibrationCb || null;
+  options.conditionCb = options.conditionCb || null;
 
   let unsucessfulCalibration = false;
   const body = [{
@@ -401,9 +403,25 @@ const ensureCalibration = (options) => {
     body.push(validateCalibration());
   }
 
+  if (options.postCalibrationCb !== null) {
+    body.push({
+      type: jsPsychHtmlKeyboardResponse,
+      stimulus: '<p style="font-size: 48px;">+</p>',
+      choices: "NO_KEYS",
+      trial_duration: 0,
+      on_start: () => {
+        options.postCalibrationCb()
+      },
+    })
+  }
+
   let calibrationsCounts;
   return {
     conditional_function() {
+      if (options.conditionCb !== null && !options.conditionCb()) {
+        return false;
+      }
+
       return !rastoc.isCorrectlyCalibrated || options.forceCalibration;
     },
     on_timeline_start() {
